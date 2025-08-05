186eefc32b1678f14fec9a53ada8861cf9011811
kdmdnd228-dot <kdmdnd228@gmail.com>
2025-08-05 08:50:50 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~om9XzADY#Zg4rKEghKPlIc1cSw7Xr2Y--OMljMqto2fAsywx3rwA",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "94752611772", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
